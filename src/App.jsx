761dffad205429e9f7d7244c153ce6060f5070b6
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-08-01 18:03:56 -0500

    feat: complete logic implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_IMG = 'https://cataas.com/cat/says/'
+
 export function App () {
-  return <h1>App de gatitos</h1>
+  const [fact, setFact] = useState()
+  const [img, setImg] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+        const firstThreeWords = fact.split(' ').slice(0, 3).join(' ')
+        fetch(CAT_ENDPOINT_IMG + `${firstThreeWords}`)
+          .then(res => res.blob())
+          .then(data => {
+            const objectUrl = URL.createObjectURL(data)
+            setImg(objectUrl)
+          })
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {img && <img src={img} alt={`Image extracted using the first three words for ${fact}`} />}
+    </main>
+  )
 }
