5baabc10db3b86f306792ee5b5314a803d7e4274
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-08-01 18:11:47 -0500

    Refactor: Split useEffect into two separate hooks

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,19 @@ export function App () {
       .then(data => {
         const { fact } = data
         setFact(fact)
-        const firstThreeWords = fact.split(' ').slice(0, 3).join(' ')
-        fetch(CAT_ENDPOINT_IMG + `${firstThreeWords}`)
-          .then(res => res.blob())
-          .then(data => {
-            const objectUrl = URL.createObjectURL(data)
-            setImg(objectUrl)
-          })
       })
   }, [])
+
+  useEffect(() => {
+    if (!fact) return
+    const firstThreeWords = fact.split(' ').slice(0, 3).join(' ')
+    fetch(CAT_ENDPOINT_IMG + `${firstThreeWords}`)
+      .then(res => res.blob())
+      .then(data => {
+        const objectUrl = URL.createObjectURL(data)
+        setImg(objectUrl)
+      })
+  }, [fact])
 
   return (
     <main>
